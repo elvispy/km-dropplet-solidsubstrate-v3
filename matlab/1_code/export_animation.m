91ae9d6f4f1f0c1d4b5947e6f81f49e08aa5eed2
Elvis Aguero <elvisavfc65@gmail.com>
2024-11-18 15:30:44 -0500

    updated export animation

--- matlab/1_code/export_animation.m
+++ matlab/1_code/export_animation.m
@@ -11,10 +11,10 @@ function export_animation(varargin)
         load(fullfilepath, "recorded_conditions", "is_adim", "default_physical");
         
         vidObj = VideoWriter(replace(fullfilepath, ".mat", ".mp4"), "MPEG-4");
-        set(vidObj, 'Quality', 100, 'FrameRate', 10);
+        set(vidObj, 'Quality', 100, 'FrameRate', 40);
         open(vidObj);
         
-        for ii = floor(linspace(1, size(recorded_conditions, 1), 200))
+        for ii = floor(linspace(1, size(recorded_conditions, 1), 600))
             adim_conditions = recorded_conditions{ii};
             if exist('is_adim') == 0 || is_adim == false
                 load(fullfilepath, "length_unit", "velocity_unit", "pressure_unit"); 
@@ -26,6 +26,8 @@ function export_animation(varargin)
             tic = sqrt(default_physical.rhoS* default_physical.undisturbed_radius^3/default_physical.sigmaS);
             %recorded_conditions{ii}.amplitude_defor = 1;
             plot_condition(1, adim_conditions, 1.25);
+            fill([-10, 10, 10, -10], [0, 0, -1, -1] * 1e6, [235 176 0]./256, 'EdgeColor', 'none', 'FaceAlpha',0.3);
+
             % title(sprintf('t = %.4f (ms), CM velocity = %.2f (cm/s), Contact Points = %g',...
             %     adim_conditions.current_time*1000, recorded_conditions{ii}.center_of_mass_velocity, ...
             %     adim_conditions.contact_points), ...
